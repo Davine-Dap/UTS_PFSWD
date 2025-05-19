2bc32b749ef536ce3a19d46175f22f82fcfd0adf
Davine-Dap <davinechen100@gmail.com>
2025-05-19 19:43:56 +0700

    layout.php

--- app/Views/layouts/layout.php
+++ app/Views/layouts/layout.php
@@ -1,24 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title><?= $this->renderSection('title') ?: 'MyApp'; ?></title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<!-- Bootstrap -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-rel="stylesheet">
+    <meta charset="UTF-8">
+    <title><?= $this->renderSection('title') ?: 'MyApp'; ?></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-<!-- Header -->
-<?= $this->include('layouts/header'); ?>
-<!-- Main Content -->
-<main class="container">
-<?= $this->renderSection('content'); ?>
-</main>
-<!-- Footer -->
-<?= $this->include('layouts/footer'); ?>
-<script
-src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></s
-cript>
+    <?= $this->include('layouts/header'); ?>
+    <main class="container">
+        <?= $this->renderSection('content'); ?>
+    </main>
+    <?= $this->include('layouts/footer'); ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
